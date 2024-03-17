e4a53b4665fabe64da8c2396ceb4458f8664ac07
Gabriella Martin <gabriella.martin0101@gmail.com>
2024-03-17 11:05:14 +0000

    feat(setup): :sparkles: retrieve and store user's favourite colour

--- src/assistant_cli/initial_setup.py
+++ src/assistant_cli/initial_setup.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import json
 from pathlib import Path
 
+from simple_term_menu import TerminalMenu
+
 SRC = Path(__file__).parent.parent
 DATA = SRC / "data" / "config.json"
 
@@ -38,3 +40,43 @@ def retrieve_name(DATA: Path = DATA) -> str:
         name = json.load(f)["name"]
     return validate_name(name)
 
+
+def favourite_colour() -> str:
+    """Prompt the user for their favourite colour."""
+    options = [
+        "black",
+        "red",
+        "green",
+        "yellow",
+        "blue",
+        "purple",
+        "cyan",
+        "white",
+        "gray",
+    ]
+    terminal_menu = TerminalMenu(
+        options,
+        title="What do you want your colour theme to be?",
+        menu_cursor="> ",
+        menu_cursor_style=("fg_black",),
+    )
+    menu_entry_index = terminal_menu.show()
+    return options[menu_entry_index]
+
+
+def persist_favourite_colour(colour: str, DATA: Path) -> None:
+    """Persist the user's favourite colour to the config file."""
+    with open(DATA, "r+") as f:
+        data = json.load(f)
+        data["colour"] = colour
+    json.dump(data, open(DATA, "w"))
+
+
+def retrieve_favourite_colour(DATA: Path = DATA) -> str:
+    """Retrieve the user's favourite colour from the config file."""
+    with open(DATA, "r") as f:
+        colour = json.load(f)["colour"]
+    if not colour:
+        colour = favourite_colour()
+        persist_favourite_colour(colour, DATA)
+    return colour
